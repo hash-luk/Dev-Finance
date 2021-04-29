e3c0439353745d60db69d647cceb6ebf653100e6
hash-luke <lucas.patrick2506@gmail.com>
2021-04-29 17:18:46 -0300

    Update JS

--- scripts.js
+++ scripts.js
@@ -1,8 +1,64 @@
-const modal = {
-    openModal() {
-        document.querySelector('.modal-overlay').classList.add('active')
+const Modal = {
+    toggleModal() {
+        document.querySelector(".modal-overlay").classList.toggle("active");
     },
-    close() {
-        document.querySelector('.modal-overlay').classList.remove('active')
-    }
-}
+};
+
+const transactionsList = [{
+        id: 1,
+        description: "Luz",
+        amount: -50000,
+        date: "23/01/2021",
+    },
+    {
+        id: 1,
+        description: "Criaçao Website",
+        amount: -70000,
+        date: "23/01/2021",
+    },
+    {
+        id: 1,
+        description: "internet",
+        amount: -20000,
+        date: "23/01/2021",
+    },
+];
+
+const Transaction = {
+    incomes() {},
+    expenses() {},
+    total() {
+        //incomes - expenses
+    },
+};
+
+const DOM = {
+
+    transactionContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = (document.createElement('tr'))
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+        DOM.transactionContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction) {
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="expense">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+              <img src="./assets/minus.svg" alt="Remover trasação" />
+            </td>
+        `;
+
+        return html;
+    },
+};
+
+transactionsList.forEach((transaction) => {
+    DOM.addTransaction(transaction);
+})
+
+
